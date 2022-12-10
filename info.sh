0a41a73e19025d8e0477d9d7fab54c4a39ab715d
13gbc <118615125+13gbc@users.noreply.github.com>
2022-12-10 12:47:18 +0100

    Update info.sh

--- info.sh
+++ info.sh
@@ -1,26 +1,37 @@
 #!/bin/bash
 
 # Display a list of logged-in users
-echo -e "\n########################################"
-echo -e "Logged-in users:\n"
-who
+logged_in_users=$(who)
 
 # Display the last 5 logins
-echo -e "\n########################################"
-echo -e "Last 5 logins:\n"
-last -n 5
+last_logins=$(last -n 5)
 
 # Display accounts with a user ID of zero
-echo -e "\n########################################"
-echo  -e "Accounts with a user ID of zero:\n"
-grep ':x:0:' /etc/passwd
+uid_zero_accounts=$(grep ':x:0:' /etc/passwd)
 
 # Display the last 5 lines of the current user's Bash history
-echo -e "\n########################################"
-echo  -e "Last 5 lines of Bash history:\n"
-tail -n 5 ~/.bash_history
+bash_history=$(tail -n 5 ~/.bash_history)
 
 # Display the files open by the current user
+open_files=$(lsof -u $(whoami))
+
+# Print the results
+echo -e "\n########################################"
+echo -e "Logged-in users:\n"
+echo "$logged_in_users"
+
+echo -e "\n########################################"
+echo -e "Last 5 logins:\n"
+echo "$last_logins"
+
+echo -e "\n########################################"
+echo  -e "Accounts with a user ID of zero:\n"
+echo "$uid_zero_accounts"
+
+echo -e "\n########################################"
+echo  -e "Last 5 lines of Bash history:\n"
+echo "$bash_history"
+
 echo -e "\n########################################"
 echo  -e "Files open by the current user:\n"
-lsof -u $(whoami)
+echo "$open_files"
