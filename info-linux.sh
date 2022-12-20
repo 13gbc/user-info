ff88593814e485625613475c3265897b8f6dc188
13gbc <118615125+13gbc@users.noreply.github.com>
2022-12-20 16:52:35 +0100

    Update info-linux.sh

--- info-linux.sh
+++ info-linux.sh
@@ -4,7 +4,7 @@
 logged_in_users=$(who)
 
 # Display the last 5 logins
-last_logins=$(last -n 5)
+last_logins=$(last -Faiwx -n 10)
 
 # Display accounts with a user ID of zero
 uid_zero_accounts=$(grep ':x:0:' /etc/passwd)
@@ -16,22 +16,33 @@ bash_history=$(tail -n 5 ~/.bash_history)
 open_files=$(lsof -u $(whoami))
 
 # Print the results
-echo -e "\n########################################"
+date
+hostname
+id
+echo -e "\n################################################################################"
 echo -e "Logged-in users:\n"
 echo "$logged_in_users"
 
-echo -e "\n########################################"
+echo -e "\n################################################################################"
 echo -e "Last 5 logins:\n"
 echo "$last_logins"
 
-echo -e "\n########################################"
+echo -e "\n################################################################################"
 echo  -e "Accounts with a user ID of zero:\n"
 echo "$uid_zero_accounts"
 
-echo -e "\n########################################"
-echo  -e "Last 5 lines of Bash history:\n"
-echo "$bash_history"
+echo -e "\n################################################################################"
+echo  -e "/etc/passwd\n"
+cat /etc/passwd
 
-echo -e "\n########################################"
+echo -e "\n################################################################################"
+echo  -e "Crontab:\n"
+crontab -l
+
+echo -e "\n################################################################################"
+echo  -e "/etc/passwd\n"
+cat /etc/passwd
+
+echo -e "\n################################################################################"
 echo  -e "Files open by the current user:\n"
 echo "$open_files"
